b10676c0833067bbb348d8460c5aa7fdcb987451
Luis Fernando Farji <fernando.farji@gmail.com>
2025-05-27 10:33:15 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.png'; // Asegurate de tener el logo en esta ruta
 
 export default function AppNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="#inicio">
           <img
             src={logo}
             width="40"
@@ -13,13 +13,15 @@ export default function AppNavbar() {
             className="d-inline-block align-top me-2"
             alt="Logo"
           />
-        2026.com.ar
+          Tu Empresa
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="navbar-content" />
+        <Navbar.Collapse id="navbar-content">
           <Nav className="ms-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
             <Nav.Link href="#productos">Productos</Nav.Link>
-            <Nav.Link href="#carrito">Carrito</Nav.Link>
+            <Nav.Link href="#comocomprar">Cómo comprar</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
