afc438bbdc6ae12919abe049f211018c4ce4e8f5
ferchuqui <fernando.farji@gmail.com>
2025-05-27 15:32:43 -0300

    arreglando el 404

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CartProvider } from './context/CartProvider';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CartProvider>
   </React.StrictMode>
 );
