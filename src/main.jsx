b03ef48e2306adbe489b17b300d66b1ee0044763
ferchuqui <fernando.farji@gmail.com>
2025-05-28 01:04:16 -0300

    formulario de contacto

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { CartProvider } from './context/CartProvider';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
+import Contacto from './pages/Contacto';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
       <HashRouter>
         <App />
+        <Contacto />
       </HashRouter>
     </CartProvider>
   </React.StrictMode>
