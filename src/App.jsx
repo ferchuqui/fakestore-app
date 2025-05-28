081164995b1801c053795f51e56f01627ca6f523
ferchuqui <fernando.farji@gmail.com>
2025-05-28 00:54:59 -0300

    agregando contacto

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import CartPage from './pages/CartPage';
 import NotFound from './pages/NotFound';
 import Gracias from './pages/Gracias';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/gracias" element={<Gracias />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/contacto" element={<Contacto />} />
+        {/* Aquí puedes agregar más rutas según sea necesario */}
       </Routes>
     </>
   );
