d3ef7a874d6ef7d6f644a436887ff59740c305b7
ferchuqui <fernando.farji@gmail.com>
2025-07-13 16:14:22 -0300

    paginado

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,21 +1,45 @@
 import { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
+import Pagination from 'react-bootstrap/Pagination';
 
 export default function ProductList() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
       .then(res => res.json())
-      .then(data => setProducts(data.slice(0, 12)));
+      .then(data => setProducts(data.slice(0, 30))); // ajusta el límite si querés más productos
   }, []);
 
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentProducts = products.slice(startIndex, startIndex + itemsPerPage);
+
+  const handlePageChange = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="container mt-4">
-      <div className="row ">
-        {products.map(product => (
+    <div className="container mt-4" id="productos">
+      <div className="row">
+        {currentProducts.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
+
+      {/* Paginación */}
+      <div className="d-flex justify-content-center mt-4">
+        <Pagination>
+          {[...Array(totalPages)].map((_, i) => (
+            <Pagination.Item
+              key={i + 1}
+              active={i + 1 === currentPage}
+              onClick={() => handlePageChange(i + 1)}
+            >
+              {i + 1}
+            </Pagination.Item>
+          ))}
+        </Pagination>
       </div>
     </div>
   );
